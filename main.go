f8a9d6d48e4e3f6f089bda8215e206c317652698
DevSamSilva <fcosamueldsn@gmail.com>
2025-05-09 23:16:35 -0300

    codigo de media com a função validar nota

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func verificaNota(nota float32) bool {
+	return nota >= 0 && nota <= 10
+}
+
+func main() {
+	var numeroDeAlunos int
+	var nota float32
+	var soma float32
+
+	fmt.Println("Digite a quantidade de alunos (máximo 10):")
+	fmt.Scan(&numeroDeAlunos)
+
+	if numeroDeAlunos <= 0 {
+		fmt.Println("Número de alunos inválido")
+		log.Fatal()
+	} else if numeroDeAlunos > 10 {
+		fmt.Println("Somente até 10 alunos são permitidos")
+		log.Fatal()
+	}
+
+	for i := 1; i <= numeroDeAlunos; i++ {
+		for {
+			fmt.Printf("Digite a nota do aluno %d: ", i)
+			fmt.Scan(&nota)
+
+			if verificaNota(nota) {
+				break
+			} else {
+				fmt.Println("Nota inválida, tente novamente!")
+			}
+		}
+		soma += nota
+	}
+
+	media := soma / float32(numeroDeAlunos)
+	fmt.Printf("A média da turma é: %.1f\n", media)
+}
